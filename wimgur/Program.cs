1493fce793f76b82665b7fc73515cafb821dc585
mystith <pkm4evr@gmail.com>
2018-03-29 17:36:38 -0400

    Add project files.

---
+++ wimgur/Program.cs
@@ -0,0 +1,107 @@
+﻿using HtmlAgilityPack;
+using Imgur.API.Authentication.Impl;
+using Imgur.API.Endpoints.Impl;
+using Imgur.API.Models.Impl;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace wallhaven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                StringBuilder syntax = new StringBuilder();
+                syntax.Append("wimgur, a wallhaven / imgur album downloader"); syntax.Append(Environment.NewLine);
+                syntax.Append("syntax: wallhaven [parameters]"); syntax.Append(Environment.NewLine);
+                syntax.Append("parameters qualify as following: website,search"); syntax.Append(Environment.NewLine);
+                syntax.Append("website = either wallhaven or imgur, capitalization doesn't matter"); syntax.Append(Environment.NewLine);
+                syntax.Append("search = any valid search on wallhaven, or any valid album on imgur"); syntax.Append(Environment.NewLine);
+                Console.WriteLine(syntax.ToString());
+                return;
+            }
+
+            using (WebClient wc = new WebClient()) {
+                foreach (string s in args.Where(o => o.ToLower().Contains("wallhaven")))
+                {
+                    string search = s.Split(',')[1];
+                    if (search.StartsWith("id:"))
+                    {
+                        HtmlDocument doc = new HtmlDocument();
+                        doc.LoadHtml(wc.DownloadString($"https://alpha.wallhaven.cc/tag/{ search.Split(new string[] { "id:" }, StringSplitOptions.None)[1] }"));
+                        search = doc.DocumentNode.Descendants("h1").FirstOrDefault(o => o.HasClass("tagname")).InnerText;
+                    }
+
+                    Directory.CreateDirectory(search);
+
+                    int i = 1;
+                    while (true)
+                    {
+                        HtmlDocument doc = new HtmlDocument();
+                        doc.LoadHtml(wc.DownloadString($"https://alpha.wallhaven.cc/search?q={ search }&page={ i }"));
+                        doc.Save("TEMP1.html");
+
+                        IEnumerable<HtmlNode> list = doc.DocumentNode.SelectNodes("//*[contains(@class,'preview')]") ?? new List<HtmlNode>().AsEnumerable();
+                        if (list.Count() == 0) break;
+                        foreach (HtmlNode link in list)
+                        {
+                            doc.LoadHtml(wc.DownloadString(link.Attributes["href"].Value));
+                            doc.Save("TEMP2.html");
+
+                            HtmlNode picture = doc.DocumentNode.SelectSingleNode("//img[contains(@id,'wallpaper')]");
+                            string pictureLink = "https:" + picture.Attributes["src"].Value;
+                            string pictureName = pictureLink.Replace("https://wallpapers.wallhaven.cc/wallpapers/full/", "");
+
+                            wc.DownloadFile(pictureLink, search + "\\" + pictureName);
+
+                            Console.WriteLine(pictureName);
+                        }
+
+                        i++;
+                    }
+                }
+
+                File.Delete("TEMP1.html");
+                File.Delete("TEMP2.html");
+
+                foreach (string s in args.Where(o => o.ToLower().Contains("imgur")))
+                {
+                    string search = s.Split(',')[1];
+                    Directory.CreateDirectory(search);
+
+                    var client = new ImgurClient("569dfa57bdec8ec");
+                    var endpoint = new AlbumEndpoint(client);
+                    var album = endpoint.GetAlbumAsync(search).GetAwaiter().GetResult();
+                    foreach (Image img in album.Images)
+                    {
+                        string name = img.Id + MimeTypeMap.List.MimeTypeMap.GetExtension(img.Type).FirstOrDefault();
+                        wc.DownloadFile(img.Link, search + "\\" + name);
+                        Console.WriteLine(name);
+                    }
+                }
+            }
+
+            if (args.FirstOrDefault().Contains("merge"))
+            {
+                string dirname = string.Join(" ", args).Split(',').LastOrDefault();
+                DirectoryInfo di = Directory.CreateDirectory(dirname);
+                DirectoryInfo current = new DirectoryInfo(Environment.CurrentDirectory);
+                foreach (FileInfo fi in current.GetFiles("*.*", SearchOption.AllDirectories).Where(o => o.FullName != AppDomain.CurrentDomain.BaseDirectory))
+                {
+                    fi.MoveTo(di.FullName + "\\" + fi.Name);
+                }
+
+                foreach (DirectoryInfo dir in current.GetDirectories().Where(o => o.Name != di.Name))
+                {
+                    dir.Delete();
+                }
+            }
+        }
+    }
+}
---
+++ wimgur/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("wimgur")]
+[assembly: AssemblyDescription("a wallhaven / imgur album downloader")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("wimgur")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07346403-acc7-4122-9c9d-84d96ffdb1a5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
